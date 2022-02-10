bd45a14a0d06c06b3fbb0921618011419d3d4a02
Hugo Ferreira <hugodinis@ua.pt>
2022-02-10 00:52:18 +0000

    Pan Camera first pass

---
+++ Keep Rolling/Assets/Scripts/PanCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanCamera : MonoBehaviour
+{
+    private Vector3 central_position;
+    private Vector3 lastPosition;
+    public float mouseSensitivity;
+    public float maxDistanceX;    
+    public float maxDistanceY;
+
+    void Start()
+    {
+        central_position = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            lastPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            var delta = Input.mousePosition - lastPosition;
+            Vector3 tr = transform.position;
+            tr += new Vector3(-delta.x * mouseSensitivity, -delta.y * mouseSensitivity, 0);
+            if (Mathf.Abs( tr.x - central_position.x ) < maxDistanceX && Mathf.Abs(tr.y - central_position.y) < maxDistanceY) {
+                Debug.Log(Vector3.Distance(tr, central_position));
+                transform.Translate(-delta.x * mouseSensitivity, -delta.y * mouseSensitivity, 0);
+            }
+            lastPosition = Input.mousePosition;
+        }
+    }
+}
