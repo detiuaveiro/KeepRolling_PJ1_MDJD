35e377f0914b96c5ec7517b69311074dfcceeec9
Hugo Ferreira <hugodinis@ua.pt>
2022-02-13 01:21:21 +0000

    Change visual added

--- Keep Rolling/Assets/Scripts/VisualSelect.cs
+++ Keep Rolling/Assets/Scripts/VisualSelect.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VisualSelect : MonoBehaviour
 {
@@ -8,6 +9,16 @@ public class VisualSelect : MonoBehaviour
     public static VisualSelect instance;
     public Sprite selectedSprite;
     public Sprite notSelectedSprite;
+    public string character_selected;
+    public string chair_selected;
+    public Dictionary<string,Sprite> sprites;
+    public Image image;
+
+    [System.Serializable]
+    public class AppearanceData {
+        public List<string> all;  
+    }
+
 
     void Start()
     {
@@ -17,15 +28,31 @@ public class VisualSelect : MonoBehaviour
         }
 
         instance = this;
+        sprites = new Dictionary<string, Sprite>();
+        TextAsset jsonFile = Resources.Load<TextAsset>("Visual/appearance");
+        AppearanceData appearanceData = JsonUtility.FromJson<AppearanceData>(jsonFile.text);
+        foreach (string spriteName in appearanceData.all) {
+            Sprite sprite = Resources.Load<Sprite>($"Visual/{spriteName}");
+            sprites.Add(spriteName, sprite);
+            Debug.Log(spriteName);
+        }
+        foreach (string s in sprites.Keys) {
+            Debug.Log(s);
+        }
+        character_selected = "rapaz";
+        chair_selected = "azul";
+        image.sprite = sprites[$"{character_selected}-{chair_selected}"];
         //load character select
     }
 
-    public void ChangeCharacter()
+    public void ChangeCharacter(string id)
     {
-    
+        character_selected = id;
+        image.sprite = sprites[$"{character_selected}-{chair_selected}"];
     }
-    public void ChangeGender()
+    public void ChangeChair(string id)
     {
-
+        chair_selected = id;
+        image.sprite = sprites[$"{character_selected}-{chair_selected}"];
     }
 }
