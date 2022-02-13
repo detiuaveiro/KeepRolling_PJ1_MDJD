90614ef5288802e111a7cf7c35a1a60fe123bb87
Miguel Silva <miguel.s.silva@ua.pt>
2022-02-13 20:06:44 +0000

    Cara do personagem no story telling

--- Keep Rolling/Assets/StoryTellerManager.cs
+++ Keep Rolling/Assets/StoryTellerManager.cs
@@ -8,8 +8,19 @@ public class StoryTellerManager : MonoBehaviour
     public Text textBox;
     public string[] textLines;
     private int currentTextLine;
+    public Image image;
+
+    public Sprite[] rapazSprites;
+    public Sprite[] raparigaSprites;
+    public Sprite[] nbSprites;
+
+    private Sprite[] spritesToImage;
 
     public static StoryTellerManager instance;
+
+    private float frameRate = 0.2f;
+    private float timer;
+    private int currentFrame;
 
     void Awake()
     {
@@ -30,6 +41,26 @@ public class StoryTellerManager : MonoBehaviour
     {
         currentTextLine = 0;
         textBox.text = textLines[currentTextLine];
+
+        switch (GameManager.instance.selectedPerson)
+        {
+            case "rapaz":
+                {
+                    spritesToImage = rapazSprites;
+                    break;
+                }
+            case "rapariga":
+                {
+                    spritesToImage = raparigaSprites;
+                    break;
+                }
+            case "nb":
+                {
+                    spritesToImage = nbSprites;
+                    break;
+                }
+        }
+        image.sprite = spritesToImage[2];
         this.gameObject.SetActive(true);
     }
 
@@ -42,6 +73,17 @@ public class StoryTellerManager : MonoBehaviour
         } else
         {
             this.gameObject.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= frameRate)
+        {
+            timer -= frameRate;
+            currentFrame = (currentFrame + 1) % spritesToImage.Length;
+            image.sprite = spritesToImage[currentFrame];
         }
     }
 }
